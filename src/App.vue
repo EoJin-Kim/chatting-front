997faca65a0801aefcd75da1c7dffe778cf23e99
EoJin-Kim <linexy@naver.com>
2022-02-21 13:39:50 +0900

    Feat: 채팅 룸

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/about">About</router-link> |
-      <router-link to="/about">Chat</router-link>
+      <router-link to="/chat/rooms">Chat</router-link>
     </div>
     <router-view/>
   </div>
